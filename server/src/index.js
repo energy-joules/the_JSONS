1501a5da0a367af11704cc2461372a1cf533e247
Anteneh13 <Antenehzewdie@ufl.edu>
2026-04-21 09:36:16 -0400

    cors fix

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://kindbridge.onrender.com" || "http://localhost:5173",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
